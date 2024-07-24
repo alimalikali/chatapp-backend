325e378ca548f8a527751c54c6e8c826e07d8ca6
alimalik <alimalikali1928@gmail.com>
2024-07-24 16:36:11 +0500

    backend works

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ import express from "express";
 import { createServer } from 'http';
 import { Server } from "socket.io";
 import { v4 as uuid } from "uuid";
-import { NEW_MESSAGE } from "./constants/events.js";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
+import { getSockets } from "./lib/helper.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import adminRoute from "./routes/admin.js";
 import chatRoute from "./routes/chat.js";
 import userRoute from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import { Message } from "./models/message.js";
 
 dotenv.config({ path: "./.env" });
 const mongoURI = process.env.URI;
 const port = process.env.PORT || 500;
 export const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
 export const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
+export const userSocketIDs = new Map();
+// const onlineUsers = new Set();
+
 connectDB(mongoURI);
 // createUser(10);
 // createSingleChats(10);
@@ -41,8 +46,22 @@ app.use("/", (req, res, next) => {
   });
 });
 
+io.use((socket, next) => {
+  cookieParser()(
+    socket.request,
+    socket.request.res,
+    async (err) => await socketAuthenticator(err, socket, next)
+  );
+});
+
 io.on("connection",(socket)=>{
+  const user = {
+    _id: "asli",
+    name:"ali"
+  };
+  userSocketIDs.set(user._id.toString() , socket.id)
   console.log("user connected",socket.id);
+  console.log(userSocketIDs,";;;;");
   socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
     const messageForRealTime = {
       content: message,
@@ -60,9 +79,25 @@ io.on("connection",(socket)=>{
       sender: user._id,
       chat: chatId,
     };
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(NEW_MESSAGE, {
+      chatId,
+      message: messageForRealTime,
+    });
+    io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId });
+
+    try {
+      await Message.create(messageForDB);
+    } catch (error) {
+      throw new Error(error);
+    }
   })
 
+
+
   socket.on("disconnect", () => {
+    userSocketIDs.delete(user._id.toString());
+    // onlineUsers.delete(user._id.toString());
     console.log("user disconnected");
   });
 })
